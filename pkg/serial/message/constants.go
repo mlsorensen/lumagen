4638dc47dbb2fcb98ea52515414b4fdd93e099a3
Marcus Sorensen <marcus_sorensen@apple.com>
2021-08-15 11:23:05 -0600

    Add constants for framerate, aspect ratio, etc. Mirrors values lumagen publishes.

---
+++ pkg/serial/message/constants.go
@@ -0,0 +1,44 @@
+package message
+
+/*
+#define FRAME_RATE_23 23
+#define FRAME_RATE_59 59
+#define FRAME_RATE_24 24
+#define FRAME_RATE_60 60
+
+#define ASPECT_RATIO_235x1 235
+#define ASPECT_RATIO_240x1 240
+#define ASPECT_RATIO_220x1 220
+#define ASPECT_RATIO_16x9  178
+#define ASPECT_RATIO_185x1 185
+#define ASPECT_RATIO_143x1 143
+#define ASPECT_RATIO_4x3   133
+
+#define HDR 1
+#define SDR 0
+
+#define RES_4K 2160
+#define RES_HD 1080
+#define RES_720P 720
+#define RES_SD 480
+*/
+
+const (
+	FrameRate23 = 23
+	FrameRate59 = 59
+	FrameRate24 = 24
+	FrameRate60 = 60
+
+	AspectRatio235 = 235
+	AspectRatio240 = 240
+	AspectRatio220 = 220
+	AspectRatio178 = 178
+	AspectRatio185 = 185
+	AspectRatio143 = 143
+	AspectRatio133 = 133
+
+	Resolution4k  = 2160
+	ResolutionHD  = 1080
+	Resolution720 = 720
+	ResolutionSD  = 480
+)
